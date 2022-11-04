50cd086a6bcbca796a42081ff15414338337b14c
pulockdas <pulockkamol50@gmail.com>
2022-11-04 22:12:19 +0600

    -Little use of NLTK pakages to tokenize and cleaning the text.

--- main.py
+++ main.py
@@ -5,27 +5,18 @@ import string
 # converting the text to lower case text.
 # removing all the punctuations
 
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
 cleaned_text = text.lower().translate(str.maketrans('', '', string.punctuation))
 print(cleaned_text)
 
-tokenized_words = cleaned_text.split()
-print(tokenized_words)
-
-stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
-              "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
-              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
-              "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
-              "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
-              "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
-              "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
-              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
-              "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
-              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+tokenized_words = word_tokenize(cleaned_text)
 
 final_words = []
 
 for word in tokenized_words:
-    if word not in stop_words:
+    if word not in stopwords.words('english'):
         final_words.append(word)
 
 print(final_words)
